b3433f3510061f8d4a47c302412aa3fadaaf88f1
AndZapata <andzapata514@gmail.com>
2019-02-07 21:13:28 +0000

    Script if, then printf

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,14 +1,11 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-
-/**
- * is_positive - Check if a number is greater than 0
- * The number to be checked
+/*
+ *main = Entry point
  *
- * Return: 1 if the number is positive. 0 otherwise
+ *return 0 (success)
  */
-
 int main(void)
 {
 	int n;
@@ -16,10 +13,19 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
-		printf("%i is positive\n", n);
+/* If n is greater than 0 then prints positive */
+	{
+		printf("%d is positive\n", n);
+	}
 	if (n == 0)
-		printf("%i is zero\n", n);
+/* If n is equal to 0 then prints is zero */
+	{
+		printf("%d is zero\n", n);
+	}
 	if (n < 0)
-		printf("%i is negative\n", n);
+/* If n is less than 0 then prints is negative */
+	{
+		printf("%d is negative\n", n);
+	}
 	return (0);
 }
