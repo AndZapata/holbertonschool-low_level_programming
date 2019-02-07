9672f6844d4367f149a5831dee4474802939adcf
AndZapata <andzapata514@gmail.com>
2019-02-07 20:17:07 +0000

    script if...printf

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,7 +4,7 @@
 
 /**
  * is_positive - Check if a number is greater than 0
- * @nb: The number to be checked
+ * The number to be checked
  *
  * Return: 1 if the number is positive. 0 otherwise
  */
