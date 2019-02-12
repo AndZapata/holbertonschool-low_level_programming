0be3d26d27436e5daeabeb63da85cfdca7cb1d43
AndZapata <778@holbertonschool.com>
2019-02-12 23:12:49 +0000

    9 times table, multiplication since 0 to 9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,43 @@
+#include "holberton.h"
+/**
+ * times_table - prints numbers from 00:00 to 23:59
+ *
+ * Description: The current file will print 00:00 to 23:59
+ */
+void times_table(void)
+{
+	int x;
+	int y;
+	int z;
+
+	z = 0;
+	for (x = 0; x <= 9; x++)
+	{
+		for (y = 0; y <= 9; y++)
+		{
+			z = x * y;
+			if (x < 10 && y != 0)
+			{
+				_putchar(32);
+			}
+			if (z > 9)
+			{
+			_putchar((z / 10) + 48);
+			_putchar((z % 10) + 48);
+			}
+			else
+			{
+				_putchar(z + 48);
+			}
+			if (y == 9)
+			{
+				_putchar(10);
+			}
+			else
+			{
+				_putchar(44);
+				_putchar(32);
+			}
+		}
+	}
+}
