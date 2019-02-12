4cb5cf593de01680025c84a8238eec2fe418a722
AndZapata <778@holbertonschool.com>
2019-02-12 20:08:53 +0000

    Print the last number

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+/**
+ * print_last_digit - prints the last digit of a number
+ *
+ * @x: The operating number
+ *
+ * Description: The current file will print 0 or 1 depending if it's lowercase
+ *
+ * Return: r if it is positive, and -r if it is negative
+ */
+int print_last_digit(int x)
+{
+	if (x >= '0')
+	{
+		x = x % 10;
+		_putchar(x + '0');
+	}
+	else
+	{
+		x = x % 10;
+		x = x * -1;
+		_putchar(x + '0');
+	}
+	return (x);
+}
