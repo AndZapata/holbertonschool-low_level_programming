dd8b9b50ec57bcff3aaf313198e1e00734c1fd6a
AndZapata <andzapata514@gmail.com>
2019-02-12 19:29:24 +0000

    print last digit of in positive

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -2,13 +2,12 @@
 /**
  * print_last_digit - prints the last digit of a number
  *
- * @lastd: variable that prints the absolute value
+ * @x: variable that prints the absolute value
  *
  * Description: The current file will print 0 or 1 depending if it's lowercase
  *
  * Return: r if it is positive, and -r if it is negative
  */
-
 int print_last_digit(int x)
 {
 	if (x >= '0')
@@ -20,7 +19,7 @@ int print_last_digit(int x)
 	else
 	{
 		x = ((x % 10) * -1);
-		     _putchar(x + '0');
-		     return (x);
+		_putchar(x + '0');
+		return (x);
 	}
 }
