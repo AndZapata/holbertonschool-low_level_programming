da8bc2be6be7e05114c99079e3efa531d9b7ef6e
AndZapata <778@holbertonschool.com>
2019-02-12 14:31:43 +0000

    At the compilation shows if is a letter or not

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -2,7 +2,7 @@
 /**
  * _isalpha - select the alphabet in lowercase
  *
- * @c: variable that does something
+ * @c: variable that tell us if it's alphabet or not
  *
  *
  * Description: The current file will print 0 or 1 depending if it's lowercase
@@ -12,6 +12,12 @@
 
 int _isalpha(int c)
 {
-	return ((c >= 97 || c <= 122 || c >= 65 || c <= 90) ? 1 : 0);
-	if (c != 97 || c != 122 || c != 65 )
+	if ((c >= 97 && c <= 122 || c >= 65 && c <= 90) ? 1 : 0)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
 }
