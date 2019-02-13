500d3de9585954082527425dfb129eb4d6fe6cb9
AndZapata <778@holbertonschool.com>
2019-02-13 00:37:34 +0000

    adds a + b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+/**
+ * add - adds a + b
+ * @a: first term to add
+ * @b: Second term to add
+ * Description: adds a + b
+ * Return: a + b
+ */
+int add(int a, int b)
+
+{
+		return (a + b);
+}
