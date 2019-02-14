5fd79741734f95ee3d91e4ca6ba789190ee3cc47
AndZapata <andzapata514@gmail.com>
2019-02-14 18:37:49 +0000

    prints multiplication of two numbers

---
+++ 0x03-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+/**
+ * mul - check the code for Holberton School students.
+ *
+ * @a: The frist digit that cames from main
+ * @b: Te second digit that cames from main
+ * Decription: will return a * b
+ * Return: Always 0
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
