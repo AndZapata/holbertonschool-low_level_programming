c84e542d32320bc70068d18db7455fa61748d948
AndZapata <andzapata514@gmail.com>
2019-02-14 20:04:53 +0000

    Print 10 times numbers since 0 to 14

--- 0x03-more_functions_nested_loops/5-more_numbers.c
+++ 0x03-more_functions_nested_loops/5-more_numbers.c
@@ -1,6 +1,6 @@
 #include "holberton.h"
 /**
- * more_numbers - print numbers except for 4 and 2
+ * more_numbers - print numbers since 0 to 14
  *
  * Decription: will print from 0 to 9
  * Return: Always 0
@@ -10,7 +10,7 @@ void more_numbers(void)
 	int d1;
 	int dig2;
 
-	for (d1 = 0; d1 <= 10; d1++)
+	for (d1 = 0; d1 <= 9; d1++)
 	{
 		for (dig2 = 0; dig2 <= 14; dig2++)
 		{
