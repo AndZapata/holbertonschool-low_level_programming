a8995499927ac534ba3fb83d3967404e4582adb3
AndZapata <andzapata514@gmail.com>
2019-02-14 02:22:25 +0000

    fibonacci secuence

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: prints adds on 3 and 5 multiples
+ * Return: Always 0
+ */
+int main(void)
+{
+	int a;
+	long n;
+	long s;
+	long z;
+
+	n = 1;
+	s = 2;
+	printf("%lu, %lu", n, s);
+	for (a = 0; a < 48; a++)
+	{
+			z = n + s;
+			n = s;
+			s = z;
+			printf(", %lu", z);
+	}
+	printf("\n");
+	return (0);
+}
