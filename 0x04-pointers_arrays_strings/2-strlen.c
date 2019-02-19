f5c15557315ec87593c309b7560fb7b397e24f85
AndZapata <andzapata514@gmail.com>
2019-02-19 16:29:35 +0000

    strlen

---
+++ 0x04-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+#include <string.h>
+/**
+ * swap_int - swap the value of the a and b pointer
+ *@s: length that cames from main.c
+ * Return: Always 0.
+ */
+
+int _strlen(char *s)
+{
+	int l;
+
+	while (s[l] != '\0')
+		{
+			l++;
+		}
+	return(l);
+}
