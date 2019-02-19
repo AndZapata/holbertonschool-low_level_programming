118e4ab0ceaa08eea056620ee9fe07906d321ea2
AndZapata <andzapata514@gmail.com>
2019-02-19 14:31:58 +0000

    n = 98

---
+++ 0x04-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+
+/**
+ * reset_to_98 - Updates the value of the n pointer
+ *@n: the numbers that cames from main.c
+ * Return: Always 0.
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
