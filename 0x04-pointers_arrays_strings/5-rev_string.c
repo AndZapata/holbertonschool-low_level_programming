310462dfd8c17219307bfc14534ed62725c21a44
AndZapata <andzapata514@gmail.com>
2019-02-20 00:13:15 +0000

    rev a string in s

---
+++ 0x04-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+/**
+ * print_rev - prints a string in reverse
+ *@s: string that cames from main.c
+ * Return: Always 0.
+ */
+
+void rev_string(char *s)
+{
+	int temp = 0;
+	int len;
+	int i;
+
+	for (len = 0; s[len] != '\0'; len++)
+	{
+	}
+	for (i = 0; i < len / 2; i++)
+	{
+		temp = s[i];
+		s[i] = s[len - i - 1];
+		s[len - i - 1] = temp;
+	}
+}
