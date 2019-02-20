14107db911aea9dfe527ad6e8b16e9ec6f3b5d29
AndZapata <andzapata514@gmail.com>
2019-02-20 00:54:10 +0000

    prints multiples of 2

---
+++ 0x04-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+ * puts2 - prints all multiples of 2 from str
+ *@str: string that cames from main.c
+ * Return: Always 0.
+ */
+
+void puts2(char *str)
+{
+	int p;
+
+	while (str[p] != '\0')
+	{
+		if (str[p] % 2 == 0)
+		{
+			_putchar(str[p]);
+		}
+		p++;
+	}
+	_putchar(10);
+}
