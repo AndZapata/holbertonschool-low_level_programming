611247a290f32f4f7c755829797e2efc8a05ff93
AndZapata <andzapata514@gmail.com>
2019-02-21 22:27:56 +0000

    compare 2 strings

--- 0x05-pointers_arrays_strings/3-strcmp.c
+++ 0x05-pointers_arrays_strings/3-strcmp.c
@@ -12,14 +12,8 @@ int _strcmp(char *s1, char *s2)
 	while (s1[i] && s2[i])
 	{
 		if (s1[i] != s2[i])
-		{
 			return (s1[i] - s2[i]);
-		}
-		else
-		{
-			return (0);
-		}
-
+		i++;
 	}
 	return (0);
 }
