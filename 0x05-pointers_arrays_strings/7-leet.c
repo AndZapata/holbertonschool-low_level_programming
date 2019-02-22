3ac52d3b8fcf2418b1a8ae92fcb06c73c8ed6db1
AndZapata <andzapata514@gmail.com>
2019-02-22 04:15:03 +0000

    leet 1337

---
+++ 0x05-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+
+/**
+ * cap_string - string from lowercase to uppercase
+ *@m: string from main
+ *
+ * Return: Always 0.
+ */
+char *leet(char *m)
+{
+	int len;
+	int temp;
+	char lt[] = "aAeEoOtTlL";
+	char nm[] = "4433007711";
+
+	for (len = 0; m[len]; len++)
+	{
+		for (temp = 0; lt[temp]; temp++)
+		{
+			if (m[len] == lt[temp])
+				m[len] = nm[temp];
+		}
+	}
+	return (m);
+}
