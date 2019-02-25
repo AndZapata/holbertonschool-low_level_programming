6c2792cf5598686172784b55f41c21be7cb3e8d0
AndZapata <andzapata514@gmail.com>
2019-02-25 21:48:02 +0000

    return the string since a character

---
+++ 0x06-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+ * _strchr - locates a character in a string
+ * @s: the string
+ * @c: character
+ *
+ * Return: Nothing.
+ */
+char *_strchr(char *s, char c)
+{
+	int len;
+
+	for (len = 0; s[len]; len++)
+	{
+		if (s[len] == c)
+			return (&s[len]);
+	}
+	return (0);
+}
