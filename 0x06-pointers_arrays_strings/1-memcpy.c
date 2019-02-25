7222ce7d8c46a7982a35ee62446b3c06e496a248
AndZapata <andzapata514@gmail.com>
2019-02-25 20:18:56 +0000

    memcpy

--- 0x06-pointers_arrays_strings/1-memcpy.c
+++ 0x06-pointers_arrays_strings/1-memcpy.c
@@ -11,7 +11,7 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int len;
 
-	for (len = 0; len < n && src[len]; len++)
+	for (len = 0; len < n; len++)
 		dest[len] = src[len];
 	return (dest);
 }
