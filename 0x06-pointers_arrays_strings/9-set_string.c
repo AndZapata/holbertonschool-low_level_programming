5e25666a761a7125343dfb8dbae65383679c231a
AndZapata <andzapata514@gmail.com>
2019-02-26 04:48:51 +0000

    set a string

---
+++ 0x06-pointers_arrays_strings/9-set_string.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * set_string - something
+ * @s: an address
+ * @to: a char
+ *
+ * Return: haystack + len
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
