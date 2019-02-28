f972533ff14c7288eed262cd9e482ab54f0354ce
AndZapata <andzapata514@gmail.com>
2019-02-28 22:28:41 +0000

    the natural square root

--- 0x07-recursion/5-sqrt_recursion.c
+++ 0x07-recursion/5-sqrt_recursion.c
@@ -1,16 +1,31 @@
 #include "holberton.h"
 /**
- * _sqrt_recursion - prints the natural square
+ * aux - does the natural square
  * @n: is a integer
- *
+ * @x: counter
  * Return: always 0
  */
-int _sqrt_recursion(int n)
-{
 
-	if (n == 0)
-		return (1);
+int aux(int x, int n)
+{
+	if (x * x == n)
+		return (x);
 	else if (n < 0)
 		return (-1);
-	return (n * (_sqrt_recursion(n - 1)));
+	else if (x * x < n)
+		return (aux(x + 1, n));
+	else
+		return (-1);
+	return (0);
+}
+
+/**
+ * _sqrt_recursion - prints and call the aux function
+ * @n: a variable called from the other function
+ * Return: Always 0.
+ */
+
+int _sqrt_recursion(int n)
+{
+	return (aux(1, n));
 }
