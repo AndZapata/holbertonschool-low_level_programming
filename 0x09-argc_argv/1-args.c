d809d65b9802138bf90d73d2355b924b03529af2
AndZapata <andzapata514@gmail.com>
2019-03-03 00:35:50 +0000

    counter of arguments

---
+++ 0x09-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *@argc: Argument
+ *@argv: pointer to a string
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+
+	printf("%d\n", argc - 1);
+	(void)argv;
+
+	return (0);
+}
