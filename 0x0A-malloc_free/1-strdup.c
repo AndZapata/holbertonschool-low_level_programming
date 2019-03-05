be333f198ad5e8bd4fec338a93930f133c721087
AndZapata <andzapata514@gmail.com>
2019-03-05 21:46:56 +0000

    copy and string

--- 0x0A-malloc_free/1-strdup.c
+++ 0x0A-malloc_free/1-strdup.c
@@ -13,11 +13,12 @@ char *_strdup(char *str)
 
 	if (*str == 0)
 		return (0);
+
 	for (i = 0; str[i]; i++)
 		b = malloc(i * sizeof(str) + 1);
 	for (i = 0; str[i]; i++)
 		b[i] = str[i];
-	if (b == 0)
+	if (str > i)
 		return (0);
 	return (b);
 }
