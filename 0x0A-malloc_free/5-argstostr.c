217eab814a67969e96b5e82feb61c8a2a7559401
AndZapata <andzapata514@gmail.com>
2019-03-07 01:14:36 +0000

    concatenates an array

---
+++ 0x0A-malloc_free/5-argstostr.c
@@ -0,0 +1,39 @@
+#include "holberton.h"
+#include <stdlib.h>
+/**
+ * argstostr - concatenates two strings
+ * @ac: first string
+ * @av: Second string
+ * Return: Nothing.
+ */
+char *argstostr(int ac, char **av)
+{
+	char *a;
+	int b, c, d = 0;
+
+	if ((ac == 0) || (av == 0))
+		return (0);
+	for (b = 0; b < ac; b++)
+	{
+		for (c = 0; av[b][c]; c++)
+		{
+		}
+	}
+	a = malloc(c * b * sizeof(char *) + ac);
+	if (a == 0)
+		return (0);
+	for (b = 0; b < ac; b++)
+	{
+		for (c = 0; av[b][c]; c++)
+		{
+			a[d] = av[b][c];
+			d++;
+		}
+		if (av[b][c] == '\0')
+		{
+			a[d] = '\n';
+			d++;
+		}
+	}
+	return (a);
+}
