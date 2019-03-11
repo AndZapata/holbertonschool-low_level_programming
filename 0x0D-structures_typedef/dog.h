7ac87f8e85af8299fb2999bd4cecf4b0564fb949
AndZapata <andzapata514@gmail.com>
2019-03-11 23:44:58 +0000

    structure called dog

---
+++ 0x0D-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG
+#define DOG
+
+/**
+ * struct dog - Define the structure dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: name of the owner of the dog
+ *
+ * Description: Structure that ask some data for a dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* DOG */
