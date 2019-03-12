de13d64bcd2aaf0b99bd0d93299b2b1f472b1d62
AndZapata <andzapata514@gmail.com>
2019-03-12 01:50:02 +0000

    local library new type def

--- 0x0D-structures_typedef/dog.h
+++ 0x0D-structures_typedef/dog.h
@@ -2,20 +2,21 @@
 #define DOG
 
 /**
- * struct dog - Define the structure dog
+ * typedef struct dog - Define the new typedefinition structure dog
  * @name: name of the dog
  * @age: age of the dog
  * @owner: name of the owner of the dog
  *
  * Description: Structure that ask some data for a dog
  */
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+} dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
 
 #endif /* DOG */
