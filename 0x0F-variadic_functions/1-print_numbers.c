a4ded06a14c0e3bce17c137a303c0f1f1e8275b6
AndZapata <andzapata514@gmail.com>
2019-03-15 20:58:06 +0000

    [Feature] a function that prints all the parameters

--- 0x0F-variadic_functions/1-print_numbers.c
+++ 0x0F-variadic_functions/1-print_numbers.c
@@ -15,15 +15,17 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	int z = 0;
 
 	va_start(x, n);
-	if (separator == NULL)
-		return;
+
 	y = 0;
 	while (y < n)
 	{
 		z = va_arg(x, int);
 		printf("%i", z);
-		if (y < n - 1)
-			printf("%s", separator);
+		if (separator != NULL)
+		{
+			if (y < n - 1)
+				printf("%s", separator);
+		}
 		y++;
 	}
 	printf("\n");
