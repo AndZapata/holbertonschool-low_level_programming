e556051f1af837c6655df15e81473f6d3c209e77
AndZapata <andzapata514@gmail.com>
2019-03-22 01:21:40 +0000

    [Feature] First advance

---
+++ 0x11-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * startfunc - returns the number of elements in a linked list
+ *
+ * Return: The numbers of nodes
+ */
+
+void __attribute__((constructor)) startfunc(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
