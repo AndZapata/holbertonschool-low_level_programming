a2e7db95528fed258408f72615cdacfc1fe49902
AndZapata <andzapata514@gmail.com>
2019-03-26 14:34:45 +0000

    [Fix] a conditional

--- 0x12-more_singly_linked_lists/2-add_nodeint.c
+++ 0x12-more_singly_linked_lists/2-add_nodeint.c
@@ -15,11 +15,11 @@ listint_t *add_nodeint(listint_t **head, const int n)
 
 	new_node = malloc(sizeof(listint_t));
 	if (new_node == NULL)
+	{
+		free(new_node);
 		return (NULL);
-	if (*head == NULL)
-		new_node->next = NULL;
-	else
-		new_node->n = n;
+	}
+	new_node->n = n;
 	new_node->next = *head;
 	*head = new_node;
 	return (new_node);
