ec0fde908873e092492a1ab036fa61f7d76c6a35
AndZapata <andzapata514@gmail.com>
2019-03-26 13:27:44 +0000

    [Feature] free a list a sets null

---
+++ 0x12-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - free a list
+ * @head: elements to print
+ *
+ * Return: The numbers of nodes
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *temp;
+
+	while ((*head)->next != NULL)
+	{
+		temp = (*head)->next;
+		free(*head);
+		*head = temp;
+	}
+	free((*head)->next);
+	free(*head);
+	*head = NULL;
+}
