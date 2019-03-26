363b4d38a99f693fd2a868b7262656e5ce39abc2
AndZapata <andzapata514@gmail.com>
2019-03-26 21:57:44 +0000

    [Fix] add free

--- 0x12-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x12-more_singly_linked_lists/9-insert_nodeint.c
@@ -16,7 +16,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	new_node = malloc(sizeof(listint_t));
 	if (new_node == NULL)
-		return (NULL);
+		return (0);
 	new_node->n = n;
 
 	if (head == NULL)
@@ -25,6 +25,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	if (idx == 0)
 	{
 		new_node->next = *head;
+		*head = new_node;
 		return (new_node);
 	}
 
