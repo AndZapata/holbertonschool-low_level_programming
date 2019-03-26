31cac22d630f18ad15f80966c88ed39c9b6fbebe
AndZapata <andzapata514@gmail.com>
2019-03-26 13:24:57 +0000

    [Feature] add a node on the beggining

---
+++ 0x12-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+/**
+ * add_nodeint - adds node at the beggining of a list
+ * @head: elements to print
+ * @n: data that cames from structure
+ *
+ * Return: The numbers of nodes
+ */
+
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *new_node;
+
+
+	new_node = malloc(sizeof(listint_t));
+	if (new_node == NULL)
+		return (NULL);
+	if (*head == NULL)
+		new_node->next = NULL;
+	else
+		new_node->n = n;
+	new_node->next = *head;
+	*head = new_node;
+	return (new_node);
+}
