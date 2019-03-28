9f74461bb96f981fdd29f164e0c28b603f4ef384
AndZapata <andzapata514@gmail.com>
2019-03-28 21:30:59 +0000

    [Fix] function that converts a binary to an unsigned int

--- 0x13-bit_manipulation/0-binary_to_uint.c
+++ 0x13-bit_manipulation/0-binary_to_uint.c
@@ -1,20 +1,5 @@
 #include "holberton.h"
 /**
- * _pow_recursion - prints a factorial
- * @x: is a integer
- * @y: is the pow
- * Return: always 0
- */
-int _pow_recursion(int x, int y)
-{
-	if (y == 0)
-		return (1);
-	else if (y < 0)
-		return (-1);
-	return (x * (_pow_recursion(x, y - 1)));
-}
-
-/**
  * binary_to_uint - converts a binary number to an unsigned int
  * @b: is the string of binary numbers
  *
@@ -22,22 +7,19 @@ int _pow_recursion(int x, int y)
  */
 unsigned int binary_to_uint(const char *b)
 {
-	int len, i, k = 0;
+	int i;
 	unsigned int sum = 0;
 
 	if (!b)
 		return (0);
-	for (len = 0; b[len]; len++)
-	{ }
-	for (i = 0; i < len; i++)
+	for (i = 0; b[i]; i++)
 	{
 		if (b[i] == '1')
-			k = 1;
+			sum = (sum << 1) | 1;
 		else if (b[i] == '0')
-			k = 0;
+			sum = sum << 1;
 		else
 			return (0);
-		sum += k * (_pow_recursion(2, (len - (i + 1))));
 	}
 	return (sum);
 }
