e96ccd476f67bbcc693d4075e766d41cd36de69e
AndZapata <andzapata514@gmail.com>
2019-04-02 21:12:56 +0000

    [Fix] a conditional

--- 0x14-file_io/1-create_file.c
+++ 0x14-file_io/1-create_file.c
@@ -16,10 +16,7 @@ int create_file(const char *filename, char *text_content)
 	if (text_content == NULL)
 		return (1);
 	if (file == -1)
-	{
-		printf("File can not be created.\n");
-		exit(1);
-	}
+		return (-1);
 	while (text_content[i])
 		i++;
 	write(file, text_content, i);
