e963ccdbf991bd567f3370d3fe7103a1541c0327
AndZapata <andzapata514@gmail.com>
2019-04-03 02:25:21 +0000

    [Fix] conditions

--- 0x14-file_io/3-cp.c
+++ 0x14-file_io/3-cp.c
@@ -8,39 +8,44 @@
  */
 int main(int ac, char *av[])
 {
-	int file_from, len = 0, file_to, conf;
+	int file_from, len = 0, file_to, wf = 0, conf;
 	char buffer[1024];
 
 	if (ac != 3)
 	{
-		dprintf(2, "Usage: cp file_from file_to\n");
+		dprintf(STDERR_FILENO,
+			"Usage: cp file_from file_to\n");
 		exit(97);
 	}
 	file_from = open(av[1], O_RDONLY);
-	if (file_from == -1)
-	{
-		dprintf(2, "Error: Can't read from file %s\n", av[1]);
-		exit(98);
-	}
 	len = read(file_from, buffer, 1024);
-	if (len == -1)
+	if (file_from == -1 || len == -1)
 	{
-		dprintf(2, "Error: Can't read from file %s\n", av[1]);
+		dprintf(STDERR_FILENO,
+			"Error: Can't read from file %s\n", av[1]);
 		exit(98);
 	}
 	file_to = open(av[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
-	if (file_to == -1)
+	wf = write(file_to, buffer, len);
+	if (file_to == -1 || wf == -1)
 	{
-		dprintf(2, "Error: Can't read from file %s\n", av[2]);
+		dprintf(STDERR_FILENO,
+			"Error: Can't write to file %s\n", av[2]);
 		exit(99);
 	}
-	write(file_to, buffer, len);
-
+	len = close(file_from);
 	conf = close(file_to);
 	if (conf == -1)
 	{
-		dprintf(2, "Error: Can't close fd %d\n", conf);
+		dprintf(STDERR_FILENO,
+			"Error: Can't close fd %d\n", conf);
 		exit(100);
 	}
-	return (STDOUT_FILENO);
+	if (len == -1)
+	{
+		dprintf(STDERR_FILENO,
+			"Error: Can't close fd %d\n", len);
+		exit(100);
+	}
+	return (0);
 }
