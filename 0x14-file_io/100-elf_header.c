0f190fd6134ea39163b880ebf54a019099e5ff61
AndZapata <andzapata514@gmail.com>
2019-04-03 20:51:49 +0000

    [Feature] advanced task

---
+++ 0x14-file_io/100-elf_header.c
@@ -0,0 +1,50 @@
+#include "holberton.h"
+
+/**
+ * main - Entry point
+ * @av: name of the file to create
+ * @ac: letteres to be printed
+ * Return: Always 1 (Success).
+ */
+int main(int ac, char *av[])
+{
+	int file_from, file_to;
+	ssize_t len = 1024, wr;
+	char buffer[1024];
+
+	if (ac != 3)
+		dprintf(STDERR_FILENO,
+			"Usage: cp file_from file_to\n"), exit(97);
+	file_from = open(av[1], O_RDONLY);
+	file_to = open(av[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	len = read(file_from, buffer, 1024);
+	if (len == -1)
+		dprintf(STDERR_FILENO,
+			"Error: Can't read from file %s\n", av[1]), exit(98);
+	while (len != '\0')
+	{
+		wr = write(file_to, buffer, len);
+		if (file_to == -1 || wr != len)
+			dprintf(STDERR_FILENO,
+				"Error: Can't write to %s\n", av[2]),
+				exit(99);
+		len = read(file_from, buffer, 1024);
+		if (file_from == -1)
+			dprintf(STDERR_FILENO,
+				"Error: Can't read from file %s\n", av[1]),
+				exit(98);
+	}
+	len = close(file_to);
+	wr = close(file_from);
+	if (len == -1 || wr == -1)
+	{
+		if (len == -1)
+			dprintf(STDERR_FILENO,
+				"Error: Can't close fd %i\n", file_to);
+		if (wr == -1)
+			dprintf(STDERR_FILENO,
+				"Error: Can't close fd %i\n", file_from);
+		exit(100);
+	}
+	return (0);
+}
