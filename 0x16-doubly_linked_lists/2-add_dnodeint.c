2d1e633ba0f51cb36cf4cedbbd0afd9644a488b7
AndZapata <andzapata514@gmail.com>
2019-05-04 20:28:03 +0000

    [Fix] cases

--- 0x16-doubly_linked_lists/2-add_dnodeint.c
+++ 0x16-doubly_linked_lists/2-add_dnodeint.c
@@ -10,7 +10,7 @@
 
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
-	dlistint_t *new_node;
+	dlistint_t *new_node, *aux;
 
 	new_node = malloc(sizeof(dlistint_t));
 	if (new_node == NULL)
@@ -18,9 +18,15 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 		free(new_node);
 		return (NULL);
 	}
-	new_node->n = n;
-	new_node->next = *head;
-	*head = new_node;
-	new_node->prev = NULL;
+	if (new_node != NULL)
+	{
+		new_node->n = n;
+		new_node->next = *head;
+		new_node->prev = NULL;
+		aux = *head;
+		if (*head != NULL)
+			aux->prev = new_node;
+		*head = new_node;
+	}
 	return (new_node);
 }
