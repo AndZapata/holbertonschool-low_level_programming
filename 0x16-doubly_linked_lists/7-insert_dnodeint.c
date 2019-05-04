c35f53a03d205e2397416e5273cba49034945895
AndZapata <andzapata514@gmail.com>
2019-05-04 22:05:40 +0000

    [Feature] add a node at a given index

--- 0x16-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x16-doubly_linked_lists/7-insert_dnodeint.c
@@ -38,6 +38,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		current_node = current_node->next;
 		idx--;
 	}
+	new_node->prev =  current_node->prev;
 	new_node->next = current_node->next;
 	current_node->next = new_node;
 	return (new_node);
