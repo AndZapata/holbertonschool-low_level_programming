5bdf63953f45e1a9f7bbadf9239a1c8277bc4ffa
AndZapata <andzapata514@gmail.com>
2019-05-11 17:27:23 +0000

    [Fix] Shared library

--- 0x17-dynamic_libraries/101-make_me_win.sh
+++ 0x17-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp/101_task.so https://github.com/AndZapata/holbertonschool-low_level_programming/raw/master/0x17-dynamic_libraries/101_task.so
+wget -P /tmp/ https://github.com/AndZapata/holbertonschool-low_level_programming/raw/master/0x17-dynamic_libraries/101_task.so
 export LD_PRELOAD=/tmp/101_task.so
