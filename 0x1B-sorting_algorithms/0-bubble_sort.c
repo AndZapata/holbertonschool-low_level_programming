75081658f56d8e6ae11f8dfca7daad9935e55db3
AndZapata <andzapata514@gmail.com>
2019-06-17 21:14:03 +0000

    [Fix] condition

--- 0x1B-sorting_algorithms/0-bubble_sort.c
+++ 0x1B-sorting_algorithms/0-bubble_sort.c
@@ -13,20 +13,20 @@ void bubble_sort(int *array, size_t size)
 	size_t i, j;
 	int temp;
 
-	if (array || size > 1)
+	if (!array || size < 2)
+		return;
+	for (i = 0; i < size - 1; i++)
 	{
-		for (i = 0; i < size - 1; i++)
+		for (j = 0; j < size - 1 - i; j++)
 		{
-			for (j = 0; j < size - 1 - i; j++)
+			if (array[j] > array[j + 1])
 			{
-				if (array[j] > array[j + 1])
-				{
-					temp = array[j];
-					array[j] = array[j + 1];
-					array[j + 1] = temp;
-					print_array(array, size);
-				}
+				temp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
+				print_array(array, size);
 			}
 		}
 	}
+
 }
