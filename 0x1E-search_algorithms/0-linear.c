3fd4355561f041077f38559bbca94bd7562326b0
AndZapata <andzapata514@gmail.com>
2019-08-28 19:27:20 +0000

    [Feature] search liner algorithm

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,14 +1,17 @@
 #include "search_algos.h"
 
 /**
+ * linear_search - searches for a value in an array of integers
  *
- *
- *
+ * @array: pointer to the first element of the array to search in
+ * @size: number of elements in array
+ * @value:  value to search for
+ * Return: index or -1 if failed
  */
 
 int linear_search(int *array, size_t size, int value)
 {
-  size_t idx = 0;
+	size_t idx = 0;
 
 	if (!array)
 		return (-1);
